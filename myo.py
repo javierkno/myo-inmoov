b045a4ebe6280293b444ef75a45ca31a3dc28a0e
javierkno <javierkno.10@gmail.com>
2017-01-27 12:05:53 +0000

    Added hand poses

--- myo.py
+++ myo.py
@@ -13,6 +13,8 @@ class MyoThread(QtCore.QThread):
 
     myo_stop_signal = False
 
+    pose = []
+
     def __init__(self):
         QtCore.QThread.__init__(self)
         self.m = []
@@ -20,28 +22,37 @@ class MyoThread(QtCore.QThread):
     def set_myo_stop_signal(self, bool):
         self.myo_stop_signal = bool
 
+    def save_pose(self, p):
+        self.pose = p
+        print(p)
+        self.emit(QtCore.SIGNAL("pose_px(QString)"),str(p.value))
+
     def run(self):
         self.set_myo_stop_signal(False)
         self.m = myo.MyoRaw(sys.argv[1] if len(sys.argv) >= 2 else None)
         #self.m.add_arm_handler(lambda arm, xdir: print('arm', arm, 'xdir', xdir))
         #self.m.add_pose_handler(lambda p: print('pose', p))
-        self.m.add_imu_handler(lambda quat, acc, gyro: print('quat', quat, 'acc', acc, 'gyro', gyro))
+        self.m.add_pose_handler(lambda p: self.save_pose(p))
+        #self.m.add_imu_handler(lambda quat, acc, gyro: print('quat', quat, 'acc', acc, 'gyro', gyro))
         self.m.connect()
         try:
             while self.myo_stop_signal==False:
                     self.m.run(1)
-        except:
+        except Exception as e:
+            #print(str(e))
             pass
-        self.disconnect()
+        finally:
+            self.disconnect()
+            print()
 
     def disconnect(self):
         self.m.disconnect()
-        print('Disconnected')
+        print('Myo disconnected')
 
     def sleep_mode(self):
         self.set_myo_stop_signal(True)
         self.m.deep_sleep()
-        print('Deep Sleep activated')
+        print('Deep sleep activated')
 
 
 class MyForm(QtGui.QMainWindow):
@@ -51,25 +62,34 @@ class MyForm(QtGui.QMainWindow):
         self.ui = Ui_Dialog()
         self.ui.setupUi(self)
 
+        self.poses=[QtGui.QPixmap('media/rest.svg'),
+                    QtGui.QPixmap('media/make-fist.svg'),
+                    QtGui.QPixmap('media/wave-left.svg'),
+                    QtGui.QPixmap('media/wave-right.svg'),
+                    QtGui.QPixmap('media/spread-fingers.svg'),
+                    QtGui.QPixmap('media/double-tap.svg'),
+                    QtGui.QPixmap('media/pan.svg'),
+                    QtGui.QPixmap('media/rotate.svg')]
+
+        self.thread = MyoThread()
+
         QtCore.QObject.connect(self.ui.btn_connect,QtCore.SIGNAL("clicked()"), self.myo_run)
         QtCore.QObject.connect(self.ui.btn_disconnect,QtCore.SIGNAL("clicked()"), self.myo_disconnect)
         QtCore.QObject.connect(self.ui.btn_sleep,QtCore.SIGNAL("clicked()"), self.myo_deep_sleep)
 
+        self.connect(self.thread, QtCore.SIGNAL("pose_px(QString)"), self.set_pose_px)
+
         self.init_buttons(False)
-
-        self.px_tap = QtGui.QPixmap('media/double-tap.svg')
-        self.px_fist = QtGui.QPixmap('media/make-fist.svg')
-        self.px_pan = QtGui.QPixmap('media/pan.svg')
-        self.px_rotate = QtGui.QPixmap('media/rotate.svg')
-        self.px_spread = QtGui.QPixmap('media/spread-fingers.svg')
-        self.px_wleft = QtGui.QPixmap('media/wave-left.svg')
-        self.px_wright = QtGui.QPixmap('media/wave-right.svg')
-
         self.ui.lbl_pose.setScaledContents(True)
+        self.set_pose_px('0')
 
-        self.ui.lbl_pose.setPixmap(self.px_fist)
 
-        self.thread = MyoThread()
+    def set_pose_px(self, pose):
+        n = int(pose)
+        if n!=255:
+            self.ui.lbl_pose.setPixmap(self.poses[n])
+        else:
+            pass
 
     def init_buttons(self, bool):
         self.ui.btn_disconnect.setEnabled(bool)
@@ -92,7 +112,7 @@ class MyForm(QtGui.QMainWindow):
     def myo_disconnect(self):
         self.init_buttons(False)
         self.thread.set_myo_stop_signal(True);
-        self.thread.disconnect()
+        #self.thread.disconnect()
 
 
 if __name__ == "__main__":
